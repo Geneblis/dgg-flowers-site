6f1022b09689672620c15ac3754857a42930ebb6
parzi18 <pedromv2006@gmail.com>
2024-11-20 20:09:28 -0300

    atualização botão js
    atualizados os nomes dos botões de todas as telas do home

--- home/scripts.js
+++ home/scripts.js
@@ -1,10 +1,10 @@
 // script.js
 document.getElementById('meuBotao').addEventListener('click', function() {
     window.location.href = '../principal/idex.html'; 
-    console.log("fds")
+    console.log("aromaticas")
 });
 
 document.getElementById('fds2').addEventListener('click', function() {
     window.location.href = '../principal/idex.html'; 
-    console.log("fds2")
+    console.log("proteas")
 });
