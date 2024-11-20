eda1d20363e9652fda9e0b2aa4741d96654c7bfa
Marcos Palmeira <kinglordbr@gmail.com>
2024-11-20 19:19:48 -0300

    se mata biel c java script
    agora a pagina principal tem um carrossel q mexe

--- principal/Script.js
+++ principal/Script.js
@@ -1,15 +1,14 @@
 const list = document.querySelector(".bolas-ordenadas");
+const items = document.querySelectorAll(".item");
+const itemWidth = items[0].offsetWidth + 20; // Inclui margem
 
-const item = document.querySelector(".item");
-
-const itemWidth = item.offsetWidth + 20;
-function handleclick(direction){
-    if (direction === "previous"){
-        list.scrollBy({left: itemWidth, behavior: "smooth"})
+function handleClick(direction) {
+    if (direction === "previous") {
+        list.scrollBy({ left: -itemWidth, behavior: "smooth" });
     } else if (direction === "next") {
         list.scrollBy({ left: itemWidth, behavior: "smooth" });
     }
 }
-document.querySelector(".button--antes").addEventListener("click",()=>handleClick("previous"))
 
+document.querySelector(".button--antes").addEventListener("click", () => handleClick("previous"));
 document.querySelector(".button--prox").addEventListener("click", () => handleClick("next"));
