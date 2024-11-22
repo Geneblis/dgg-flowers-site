2c0afde70c532cf5b281e90b2ea33291d8135225
parzi18 <pedromv2006@gmail.com>
2024-11-22 20:04:45 -0300

    att pasta de botão

---
+++ home/botõesFloreSecas.js
@@ -0,0 +1,11 @@
+document.getElementById('pedidos').addEventListener('click', function() {
+    window.location.href = ''; 
+});
+
+document.getElementById('conta').addEventListener('click', function() {
+    window.location.href = '../home/index.html'; 
+});
+
+document.getElementById('carrinho').addEventListener('click', function() {
+    window.location.href = ''; 
+});
