d8155007d05e46dcf21800eaf09ffc33c35029c3
Marcos Palmeira <kinglordbr@gmail.com>
2024-11-26 19:38:03 -0300

    carrinho update

--- Mestre/carrinho/scripts.js
+++ Mestre/carrinho/scripts.js
@@ -1,8 +1,35 @@
-const buttons = document.querySelectorAll('.payment-method button');
+function atualizarCarrinho(){
+  const carrinhoDiv = document.getElementById('carrinho');
+  const totalSpan = document.getElementById('total');
 
-buttons.forEach(button => {
-  button.addEventListener('click', () => {
-    buttons.forEach(btn => btn.classList.remove('active'));
-    button.classList.add('active');
-  });
+  let carrinho = JSON.parse(localStorage.getItem('carrinho')) || [];
+  let total = 0;
+
+  carrinhoDiv.innerHTML = '';
+
+  carrinho.forEach((item, index) => {
+    carrinhoDiv.innerHTML += `
+        <div class="item-carrinho">
+            <img src="${item.imagem}" alt="${item.nome}">
+            <p>${item.nome} - R$ ${item.preco.toFixed(2)} 
+            <button class="remover" onclick="removerDoCarrinho(${index})">Remover</button></p>
+            <hr>
+        </div>
+    `;
+    total += item.preco; 
 });
+totalSpan.innerHTML = total.toFixed(2)
+}
+
+
+function removerDoCarrinho(index){
+  let carrinho = JSON.parse(localStorage.getItem('carrinho')) || [];
+
+  carrinho.splice(index, 1);
+
+  localStorage.setItem('carrinho', JSON.stringify(carrinho))
+
+  atualizarCarrinho();
+
+}
+atualizarCarrinho();
---
+++ Mestre/review/envCarrinho.js
@@ -0,0 +1,15 @@
+function adicionarAoCarrinho(button){
+    const produto = button.parentElement;
+    const nome = produto.getAttribute('data-nome');
+    const imagem = produto.getAttribute('data-imagem');
+    const preco = parseFloat(produto.getAttribute('data-preco'));
+
+    let carrinho = JSON.parse(localStorage.getItem('carrinho')) || [];
+
+    carrinho.push({nome, preco, imagem});
+
+    localStorage.setItem('carrinho', JSON.stringify(carrinho));
+
+    alert(`${nome} adicionado ao carrinho`);
+
+}
