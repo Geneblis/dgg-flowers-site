e53f92799df1eac1c93e9c88c306ca9d388d479f
Geneblis <94307472+Geneblis@users.noreply.github.com>
2024-11-27 13:00:01 -0300

    socorro

--- Mestre/review/scripts_compras.js
+++ Mestre/review/scripts_compras.js
@@ -1,33 +1,72 @@
 var modal = document.getElementById("pagamentoModal");
+var modalCartao = document.getElementById("cartaoModal");
+var modalPix = document.getElementById("pixModal");
+var modalBoleto = document.getElementById("boletoModal");
+
 var comprando = document.querySelector(".comprar");
-var metodo_pagamento_boolean = document.getElementsByClassName("fechar_pagamento")[0];
+var fecharModal = document.querySelectorAll(".fechar_pagamento");
+var cartaoCredito = document.getElementById("cartaoCredito");
+var pixButton = document.getElementById("pix");
+var boletoButton = document.getElementById("boleto");
+
+var finalizarBotoes = document.querySelectorAll(".finalizar");
 
 // Abrir o modal de pagamento
 comprando.onclick = function() {
     modal.style.display = "block";
 }
 
-// Fechar o modal de pagamento
-metodo_pagamento_boolean.onclick = function() {
-    modal.style.display = "none";
-}
+// Fechar qualquer modal ao clicar no botão de fechar
+fecharModal.forEach(function(btn) {
+    btn.onclick = function() {
+        modal.style.display = "none";
+        modalCartao.style.display = "none";
+        modalPix.style.display = "none";
+        modalBoleto.style.display = "none";
+    }
+});
 
 // Fechar o modal ao clicar fora dele
 window.onclick = function(event) {
-    if (event.target == modal) {
+    if (event.target == modal || event.target == modalCartao || event.target == modalPix || event.target == modalBoleto) {
         modal.style.display = "none";
+        modalCartao.style.display = "none";
+        modalPix.style.display = "none";
+        modalBoleto.style.display = "none";
     }
 }
 
-document.getElementById("verFAQButao").onclick = function() {
-    var conteudoFAQ = document.getElementById("faqContainer");
-    
-    // Verifica se o conteúdo FAQ está oculto ou visível
-    if (conteudoFAQ.style.display === "none" || conteudoFAQ.style.display === "") {
-        conteudoFAQ.style.display = "block";
-        this.innerText = "Ver Menos";
-    } else {
-        conteudoFAQ.style.display = "none";
-        this.innerText = "Ver Perguntas Frequentes";
+// Abrir o modal de cartão de crédito
+cartaoCredito.onclick = function() {
+    modal.style.display = "none"; 
+    modalCartao.style.display = "block"; 
+}
+
+// Abrir o modal de PIX
+pixButton.onclick = function() {
+    modal.style.display = "none"; 
+    modalPix.style.display = "block"; 
+}
+
+// Abrir o modal de Boleto
+boletoButton.onclick = function() {
+    modal.style.display = "none"; 
+    modalBoleto.style.display = "block"; 
+}
+
+// Seleciona todos os botões "Voltar" e adiciona o evento de clique
+document.querySelectorAll(".voltar").forEach(function(voltarButton) {
+    voltarButton.onclick = function() {
+        modalCartao.style.display = "none"; 
+        modalPix.style.display = "none"; 
+        modalBoleto.style.display = "none"; 
+        modal.style.display = "block"; // Volta para o modal de pagamento
     }
-};
+});
+
+//finalizar a compra
+finalizarBotoes.forEach(function(finalizarButao) {
+    finalizarButao.onclick = function() {
+        removerDoCarrinho(this);
+    };
+});
