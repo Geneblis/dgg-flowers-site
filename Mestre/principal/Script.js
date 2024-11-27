1766d49ddd837b20a2da8fde2e22ab356d347601
Marcos Palmeira <kinglordbr@gmail.com>
2024-11-27 09:34:52 -0300

    tentativa de cura de ansiedade do codigo

--- Mestre/principal/Script.js
+++ Mestre/principal/Script.js
@@ -1,3 +1,5 @@
+console.log("fds")
+
 const list = document.querySelector(".bolas-ordenadas");
 const items = document.querySelectorAll(".item");
 const itemWidth = items[0].offsetWidth + 20; // Inclui margem
@@ -5,9 +7,21 @@ const itemWidth = items[0].offsetWidth + 20; // Inclui margem
 function handleClick(direction) {
     if (direction === "previous") {
         list.scrollBy({ left: -itemWidth, behavior: "smooth" });
+        console.log("pra trasss")
     } else if (direction === "next") {
         list.scrollBy({ left: itemWidth, behavior: "smooth" });
+        console.log("pra frente")
     }
+}
+
+function praFrente(){
+    list.scrollBy({ left: -itemWidth, behavior: "smooth" });
+    console.log("pra trasss")   
+}
+function praTras(){
+    list.scrollBy({ left: itemWidth, behavior: "smooth" });
+    console.log("pra frente")
+
 }
 
 document.querySelector(".button--antes").addEventListener("click", () => handleClick("previous"));
