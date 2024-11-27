d89c3586b82069c5ba1adf54951374fcfb3af431
Marcos Palmeira <kinglordbr@gmail.com>
2024-11-27 11:14:56 -0300

    os stories voltaram

--- Mestre/principal/Script.js
+++ Mestre/principal/Script.js
@@ -1,28 +1,25 @@
-console.log("fds")
+document.addEventListener("DOMContentLoaded", () => {
+    const list = document.querySelector(".bolas-ordenadas");
+    const items = document.querySelectorAll(".item");
+    const itemWidth = items[0].offsetWidth + 20; // Inclui margem
 
-const list = document.querySelector(".bolas-ordenadas");
-const items = document.querySelectorAll(".item");
-const itemWidth = items[0].offsetWidth + 20; // Inclui margem
-
-function handleClick(direction) {
-    if (direction === "previous") {
-        list.scrollBy({ left: -itemWidth, behavior: "smooth" });
-        console.log("pra trasss")
-    } else if (direction === "next") {
-        list.scrollBy({ left: itemWidth, behavior: "smooth" });
-        console.log("pra frente")
+    function handleClick(direction) {
+        if (direction === "previous") {
+            list.scrollBy({ left: -itemWidth, behavior: "smooth" });
+            console.log("tras");
+        } else if (direction === "next") {
+            list.scrollBy({ left: itemWidth, behavior: "smooth" });
+            console.log("frente");
+        }
     }
-}
 
-function praFrente(){
-    list.scrollBy({ left: -itemWidth, behavior: "smooth" });
-    console.log("pra trasss")   
-}
-function praTras(){
-    list.scrollBy({ left: itemWidth, behavior: "smooth" });
-    console.log("pra frente")
+    document.querySelector(".antes").addEventListener("click", () => {
+        console.log("Botão anterior clicado");
+        handleClick("previous");
+    });
 
-}
-
-document.querySelector(".button--antes").addEventListener("click", () => handleClick("previous"));
-document.querySelector(".button--prox").addEventListener("click", () => handleClick("next"));
+    document.querySelector(".prox").addEventListener("click", () => {
+        console.log("Botão próximo clicado");
+        handleClick("next");
+    });
+});
