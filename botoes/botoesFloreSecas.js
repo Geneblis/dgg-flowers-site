954d60bfcf3934e669fc99aec46bb68151434476
Geneblis <94307472+Geneblis@users.noreply.github.com>
2024-12-03 23:35:03 -0300

    calma la

--- botoes/botoesFloreSecas.js
+++ botoes/botoesFloreSecas.js
@@ -3,21 +3,21 @@ document.getElementById('pedidos').addEventListener('click', function() {
 });
 
 document.getElementById('conta').addEventListener('click', function() {
-    window.location.href = '../home/index.html'; 
+    window.location.href = './home/index.html'; 
 });
 
 document.getElementById('carrinho').addEventListener('click', function() {
     window.location.href = ''; 
 });
 document.getElementById('home').addEventListener('click', function() {
-    window.location.href = '../principal/idex.html'; 
+    window.location.href = './principal/idex.html'; 
 });
 document.getElementById('seca').addEventListener('click', function() {
-    window.location.href = '../home/floreSecas.html'; 
+    window.location.href = './home/floreSecas.html'; 
 });
 document.getElementById('vaso').addEventListener('click', function() {
-    window.location.href = '../home/vasosFlores.html'; 
+    window.location.href = './home/vasosFlores.html'; 
 });
 document.getElementById('protea').addEventListener('click', function() {
-    window.location.href = '../home/floresProteas.html'; 
+    window.location.href = './home/floresProteas.html'; 
 });
